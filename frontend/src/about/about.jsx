ad4e372051b82c105b506a60492e7af9889663a8
graciani23 <gracianigss@gmail.com>
2020-06-12 10:17:56 -0300

    estruturação frontend

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dolores voluptatibus velit reprehenderit, facilis esse repellat rem sint inventore ad eum nam vel distinctio eligendi perspiciatis ut quidem pariatur sunt?</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque molestiae perspiciatis incidunt, quos error fuga officia vel eveniet voluptate ducimus libero possimus minima quas cumque accusamus? Natus officiis facere iure!</p>
+    <h2>Imprensa</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, provident. Sapiente dicta soluta est eligendi deserunt. Ullam, culpa expedita sequi consequatur aut, sit natus harum hic magni exercitationem quidem dolorem.</p>
+  </div>
+)
